c0c3697c4ba62118a6fae4959b8221a70bce054d
Phuong Tran Ha <tranhaphuong13@gmail.com>
2018-10-01 15:57:01 +0700

    1/10

--- js/cr_room.js
+++ js/cr_room.js
@@ -1,63 +1,63 @@
 $(document).ready(function(){
 	var getNameHome = localStorage.getItem("storageNameHome");
-	var idHome;
+	var dataHome;
 	var status_create;
-	var count = 1;
+	var loadRoom = 0;
+	// var count = loadRoom;
 	var dataRoomGet;
+	var listRoom;
 	$.ajax({
 			async : false,
 			method: "get",
 			contentType: "application/json",
 			url: "http://localhost:8080/smarthome/gethome/"+getNameHome,
 	}).done(function(data, textStatus, xhr){
-		idHome = data;
-		//alert(idHome.id);
+		dataHome = data;
 	});
 
-	// $.ajax({
-	// 		async : false,
-	// 		method: "get",
-	// 		contentType: "application/json",
-	// 		url: "http://localhost:8080/smarthome/getlistrooms/"+getNameHome,
-	// }).done(function(data, textStatus, xhr){
-	// 	//idHome = data;
-	// 	// alert(data[0].nameRoom);
-	// });
+	$.ajax({
+			async : false,
+			method: "get",
+			contentType: "application/json",
+			url: "http://localhost:8080/smarthome/getlistrooms/"+ getNameHome,
+	}).done(function(data, textStatus, xhr){
+		listRoom = data;
+	});
 
-  	$(".button-add").click(function(){
-  		
-  		$(".row").append("<div class='col-sm-3 room rroom"+count+"'></div>");
-  		$(".rroom"+count).append("<div class='hovereffect hover"+count+"'></div>");
-  		$(".hover"+count).append("<img class='img-fluid img-room' src='images/room-icon.jpg'>");
-  		$(".hover"+count).append("<div class='overlay olay"+count+"'></div>");
-  		$(".olay"+count).append("<input id='name_input"+count+"' name='name_input' type ='text' placeholder = 'Enter name room'>");
-  		$(".olay"+count).append("<input id='id_input"+count+"' type='text'>0");
-  		$(".olay"+count).append("<p class= 'adbtn"+count+"'></p>");
-  		$(".adbtn"+count).append("<button class = 'btn_enter"+count+"'>Enter Room</button>");
-  		$(".adbtn"+count).append("<button class = 'btn_enter' id= 'btn-save"+count+"'>Save</button>");
-  		$("#btn-save"+count).click(function(){
+	function appentRoom(countRoom){
+		$(".row").append("<div class='col-sm-3 room rroom"+countRoom+"'></div>");
+  		$(".rroom"+countRoom).append("<div class='hovereffect hover"+countRoom+"'></div>");
+  		$(".hover"+countRoom).append("<img class='img-fluid img-room' src='images/room-icon.jpg'>");
+  		$(".hover"+countRoom).append("<div class='overlay olay"+countRoom+"'></div>");
+  		$(".olay"+countRoom).append("<input id='name_input"+countRoom+"' name='name_input' type ='text' placeholder = 'Enter name room'>");
+  		$(".olay"+countRoom).append("<input id='id_input"+countRoom+"' type='hidden'>");
+  		$(".olay"+countRoom).append("<p class= 'adbtn"+countRoom+"'></p>");
+  		$(".adbtn"+countRoom).append("<button class = 'btn_enter"+countRoom+"'>Enter Room</button>");
+  		$(".adbtn"+countRoom).append("<button class = 'btn_enter' id= 'btn-save"+countRoom +"'>Save</button>");
+	}
+
+	function save(saveCount){
+		  	$("#btn-save"+saveCount).click(function(){
   			var nameRoom = $("#name_input"+$(this).attr("id").substr(-1)).val();
   			var idRoom = parseInt($("#id_input"+$(this).attr("id").substr(-1)).val());
   			if(isNaN(idRoom)|| idRoom == null){
   				idRoom = 0;
   			}
-		/*********************************************************************************/
-			// Begin save request
+
 		    $.ajax({
 				async : false,
 				method: "post",
-				data: JSON.stringify({ id: idRoom, homeId:idHome, nameRoom:nameRoom }),
+				data: JSON.stringify({ id: idRoom, homeId:dataHome, nameRoom:nameRoom }),
 				contentType: "application/json",
 				url: "http://localhost:8080/smarthome/createroom/"+ idRoom
 			}).done(function(data, textStatus, xhr){
 				status_create = xhr.status;
 			});
-			//End save request
-		/**********************************************************************************/
+		});
+	}
 
-		/*********************************************************************************/
+	function getRoom(getCount){
 		if(status_create == 201){
-			// Begin get request
 		    $.ajax({
 			async : false,
 			method: "get",
@@ -66,15 +66,23 @@ $(document).ready(function(){
 			}).done(function(data, textStatus, xhr){
 				dataRoomGet = data;
 			});
-			//End get request
 			
 			// Set value for fields value 
-			$("#name_input"+$(this).attr("id").substr(-1)).val(dataRoomGet.nameRoom);
-			$("#id_input"+$(this).attr("id").substr(-1)).val(dataRoomGet.id);
+			$("#name_input"+getCount).val(dataRoomGet.nameRoom);
+			$("#id_input"+getCount).val(dataRoomGet.id);
 		}
-		/**********************************************************************************/
+	}
+	for(loadRoom; loadRoom < listRoom.length; loadRoom++){
+		appentRoom(loadRoom);
+		$("#name_input"+loadRoom).val(listRoom[loadRoom].nameRoom);
+		$("#id_input"+loadRoom).val(listRoom[loadRoom].id);
+		save(loadRoom);
+	}
 
-		});
-		count++;
+  	$(".button-add").click(function(){
+  		appentRoom(loadRoom++);
+ 		save(loadRoom++);
+ 		getRoom(loadRoom++)
+		loadRoom++;
   	});
 })
